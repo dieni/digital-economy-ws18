dcec134f7b8fc32c060ecdfd989355a975e42679
Christian Dienbauer <christian.dienbauer@gmx.at>
2019-01-17 16:40:39 +0100

    b2b storno finished. also some form logic created. athorization b2b, get all orders from a specific user.

--- api.py
+++ api.py
@@ -4,6 +4,9 @@ from data import db_connection
 import xml.etree.ElementTree as et
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '24446f27d7e8540adcaf914f0aea6bd7'
+
+
 db = db_connection()
 
 # B2C Webshop
@@ -31,9 +34,8 @@ def products():
     '''
     Here the user can see all products which are available.
     '''
-    products = db.get_products
-
-    # return "products"
+    # TODO: Modify the template to check some products to buy. Use a Button to proceed with purchasing.
+    products = db.get_products()  # get products from the database
     return render_template('products.html', title='Products', products=products)
 
 
@@ -90,13 +92,81 @@ def api_search():
 
 @app.route('/api/orders', methods=['GET', 'PUT'])
 def cancellation():
-    # TODO
+    '''
+    FINISHED
+    Customer must be authorized.
+
+    GET: Get a list of all orders of a user
+    PUT: Cancel a specific order
+    '''
+    # Authorization
+    customer = authorize(request)
+
     if request.method == 'PUT':
         # cancel order and send back confirmation.
-        return "confirmation"
+
+        # reading the xml from the request
+        root = et.fromstring(request.data)
+
+        # get order id from request
+        order_id = root.findall(".//bestellungid")  # returns a list
+
+        # cancel order
+        return db.cancel_order(customer['customer_id'], int(order_id[0].text))
+
     else:
         # return a list of orders from a user
-        return "List of order"
+        orders = db.get_orders(customer['customer_id'])
+
+        # building xml
+        root = et.Element('orders')
+
+        for o in orders:
+            order = et.SubElement(root, 'order')
+            order_id = et.SubElement(order, 'id')
+            order_id.text = str(o['order_id'])
+            customer_id = et.SubElement(order, 'customer_id')
+            customer_id.text = str(o['customer_id'])
+            payment_id = et.SubElement(order, 'payment_id')
+            payment_id.text = str(o['payment_id'])
+
+            # build all products in an order
+            products = et.SubElement(order, 'products')
+            for p in o['products']:
+                product = et.SubElement(products, 'product')
+                product_id = et.SubElement(product, 'product_id')
+                product_id.text = str(p['product_id'])
+                quantity = et.SubElement(product, 'quantity')
+                quantity.text = str(p['quantity'])
+
+            bought = et.SubElement(order, 'bought')
+            bought.text = str(o['bought'])
+            canceled = et.SubElement(order, 'canceled')
+            canceled.text = str(o['canceled'])
+
+    # creating from a string
+    xml_str = et.tostring(root, encoding='utf8', method='xml')
+
+    return Response(xml_str, mimetype='text/xml')
+
+
+def authorize(request):
+    '''
+    This is a method to authorize a customer. If login is missing or is wrong response with error message will sent. Else this
+    method returns the customer object.
+    '''
+    auth = request.authorization
+
+    if not auth:
+        return "You are not authorized!"
+
+    customer = db.authorize(int(auth.username), auth.password)
+
+    if not customer:
+        return "Wrong customer id or password!"
+
+    # if authorization is ok return object of customer
+    return customer
 
 
 if __name__ == '__main__':
