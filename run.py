1c7d7a681d3d6d2fcdc8c4d579c74484ea280f52
Christian Dienbauer <christian.dienbauer@gmx.at>
2019-01-20 22:53:05 +0100

    update ssl

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
 from fishshop import app
-from OpenSSL import SSL
-context = SSL.Context(SSL.SSLv23_METHOD)
-context.use_privatekey_file('fish_cert.pem')
-context.use_certificate_file('fish_key.pem')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-    # app.run(ssl_context='adhoc', debug=True) # uses dummy certificate
-    # uses openssl certificate
-    # app.run(host='127.0.0.1', port='5000', debug=True, ssl_context=context)
+    # app.run(ssl_context='adhoc', debug=True)  # uses dummy certificate
